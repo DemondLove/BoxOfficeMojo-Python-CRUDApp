687894ac6680ca8214396d30713cb64da2ab901a
DemondLove <dhlove1s@gmail.com>
2021-09-27 16:55:27 -0700

    cleanup app directory

--- views/models/title.py
+++
@@ -1,7 +0,0 @@
-from ._helper import db
-
-class Title(db.Model):
-    __tablename__: 'title'
-    
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(), nullable=False, index=True, unique=True)
