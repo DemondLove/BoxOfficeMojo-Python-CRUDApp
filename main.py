4d1f9184f977b465151fb5f7dae8a562bc6f0f01
DemondLove <dhlove1s@gmail.com>
2021-09-27 17:22:35 -0700

    add config file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from bom import app, db
+import os
+from bom import create_app, db
 from bom.models import Title
+from flask_migrate import Migrate
 
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run()
+@app.shell_context_processor
+def makeShellContext():
+    return dict(db=db, Title=Title)
