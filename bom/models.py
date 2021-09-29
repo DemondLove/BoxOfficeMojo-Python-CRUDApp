9f9499dad88e767504f2be3b120bcc5496fdb188
DemondLove <dhlove1s@gmail.com>
2021-09-28 17:58:10 -0700

    update getTitleByID

--- bom/models.py
+++ bom/models.py
@@ -5,3 +5,12 @@ class Title(db.Model):
     
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     title = db.Column(db.String(), nullable=False, index=True, unique=True)
+
+    # def getTitleByID(id):
+    #     response = self.query.filter_by(id=id).first()
+    #     return response.title
+
+    @classmethod
+    def getTitleByID(cls, id):
+        response = Title.query.filter_by(id=id).first()
+        return response.title
